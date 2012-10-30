8b2cf66386cd85f05699d055d254215c1e30ba4f
Vanderson Mota <vanderson.mota@gmail.com>
2012-10-30 16:46:00 -0200

    avoiding CString

--- lib/algorithms/string.rb
+++ lib/algorithms/string.rb
@@ -1,10 +1,10 @@
 =begin rdoc
     This module implements string algorithms. Documentation is provided for each algorithm.
-    
+
 =end
 
 
 begin
-	require 'CString' unless RUBY_PLATFORM =~ /java/
+ #require 'CString' unless RUBY_PLATFORM =~ /java/
 rescue
 end
